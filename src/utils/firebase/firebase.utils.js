14bafef849e527cb87bca7a2432919b14a008433
Charlene <charlenegipulan@gmail.com>
2022-06-29 22:03:54 -0700

    implement create user

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -5,6 +5,7 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
 } from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyBWvrvRMfNv-tSSW75BW7YYBXhtzbHfgGs',
@@ -23,5 +24,41 @@ provider.setCustomParameters({
   prompt: 'select_account',
 });
 
+//instantiates firebase auth
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+//ceate db and instiate firebase database
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot.exists());
+
+    //check if user snapshot exist, if not set it in our database
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+  // if user data exist
+
+  //if user data does not exist
+
+  // retur back user Doc
+};
